98d3b141d5c1cc917b840782723ea9e1de5f2880
i9sistemasesolucoes-max <i9sistemasesolucoes@gmail.com>
2026-04-29 20:02:54 -0300

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,24 +1,7 @@
-<?php
-
-use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Configuration\Exceptions;
-use Illuminate\Foundation\Configuration\Middleware;
-
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php', // ESTA LINHA ESTAVA FALTANDO
+        api: __DIR__.'/../routes/api.php', // ADICIONE ESTA LINHA EXATAMENTE AQUI
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
-        // Isso impede que o Laravel barre o POST da Base44/SellerX
-        $middleware->validateCsrfTokens(except: [
-            'api/*',
-            'api/nfe/*',
-            'api/nfe/emitir'
-        ]);
-    })
-    ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
