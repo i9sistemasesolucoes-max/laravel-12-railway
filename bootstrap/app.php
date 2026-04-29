d18278957019d87e4ae376f818d2e5fe6f1f3482
i9sistemasesolucoes-max <i9sistemasesolucoes@gmail.com>
2026-04-29 16:35:30 -0300

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,9 +10,12 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+        // Isso remove a barreira de segurança que está causando o erro 500
+        $middleware->validateCsrfTokens(except: [
+            'api/nfe/*', 
+        ]);
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
