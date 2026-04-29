13597cda8b0462f6e90fde0a7a9de5e0885bc653
i9sistemasesolucoes-max <i9sistemasesolucoes@gmail.com>
2026-04-29 19:43:01 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,3 +1,6 @@
+<?php
+// FORÇAR EXIBIÇÃO DE ERROS - ADICIONE ISSO AQUI:
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+define('LARAVEL_START', microtime(true));
