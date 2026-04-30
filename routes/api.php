18af8c0ca28a2748f7a974448d4aa166f8207c46
i9sistemasesolucoes-max <i9sistemasesolucoes@gmail.com>
2026-04-30 14:26:40 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,7 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::any('/nfe/emitir', function () {
-    return response()->json([
-        'status' => 'sucesso',
-        'motor' => 'Neuraif Online'
-    ]);
-});
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
