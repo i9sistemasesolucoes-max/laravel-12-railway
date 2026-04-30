6e9df2bbe8ddec18d9e9b9db966a7d319b455ee2
i9sistemasesolucoes-max <i9sistemasesolucoes@gmail.com>
2026-04-30 15:32:01 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return response()->json(['status' => 'online', 'motor' => 'Neuraif Online']);
+Route::get('/limpar-cache', function () {
+    try {
+        \Illuminate\Support\Facades\Artisan::call('optimize:clear');
+        return response('<h1>Cache destruído com sucesso. O motor está limpo.</h1>', 200);
+    } catch (\Exception $e) {
+        return response('Erro ao limpar cache: ' . $e->getMessage(), 500);
+    }
 });
